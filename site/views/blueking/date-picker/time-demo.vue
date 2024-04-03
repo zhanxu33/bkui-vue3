6dc8c3c959064504ac49d130f3c7d02559be507e
liangling <liangling@tencent.com>
2024-04-03 15:53:09 +0800

    feat: 更新业务组件date-picker # Reviewed, transaction id: 5134

--- site/views/blueking/date-picker/time-demo.vue
+++ site/views/blueking/date-picker/time-demo.vue
@@ -21,19 +21,19 @@
   />
 </template>
 <script setup lang="ts">
-  import { ref } from 'vue';
+import { ref } from 'vue';
 
-  import DatePicker from '@blueking/date-picker';
+import DatePicker from '@blueking/date-picker';
 
-  import '@blueking/date-picker/dist/vue3-light.css';
-  const recent = ref(['now-2d', 'now']);
-  const future = ref(['now', 'now+2d']);
-  const natural = ref(['now-1w/w', 'now-1w/w']);
-  const range = ref(['2024-03-11 00:20:00', 'now']);
-  const range2 = ref(['2024-03-11 00:20:00', '2024-06-11 00:20:00']);
+import '@blueking/date-picker/vue3/vue3.css';
+const recent = ref(['now-2d', 'now']);
+const future = ref(['now', 'now+2d']);
+const natural = ref(['now-1w/w', 'now-1w/w']);
+const range = ref(['2024-03-11 00:20:00', 'now']);
+const range2 = ref(['2024-03-11 00:20:00', '2024-06-11 00:20:00']);
 </script>
 <style>
 .common-picker {
-  margin: 0 10px
+  margin: 0 10px;
 }
 </style>
