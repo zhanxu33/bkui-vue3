8f9ca714f57862bec011913603ac819e7bcae33b
hito <hubu_zht@163.com>
2024-07-09 16:49:36 +0800

    feature(select|resize): 完善select和resize文档 (#1994)

--- site/views/select/select.tsx
+++ site/views/select/select.tsx
@@ -126,6 +126,20 @@ const propsJson: IPropsTableItem[] = [
     optional: [],
   },
   {
+    name: 'showAll',
+    type: 'boolean',
+    default: 'false',
+    desc: '是否展示全部',
+    optional: ['true', 'false'],
+  },
+  {
+    name: 'allOptionId',
+    type: 'string|number',
+    default: '',
+    desc: 'showAll为true时，全部选项的ID',
+    optional: [],
+  },
+  {
     name: 'showSelectAll',
     type: 'boolean',
     default: 'false',
@@ -363,6 +377,11 @@ const propsJson: IPropsTableItem[] = [
 ];
 const eventJson = [
   {
+    name: 'update:modelValue',
+    desc: 'v-model事件',
+    params: 'value',
+  },
+  {
     name: 'change',
     desc: '选中值发生变化时触发',
     params: 'value',
@@ -406,6 +425,11 @@ const eventJson = [
     name: 'deselect',
     desc: '取消选中时调用',
     params: 'value',
+  },
+  {
+    name: 'search-change',
+    desc: '自定义搜索回调',
+    params: 'searchValue',
   },
 ];
 // 输入框插槽
