d63d44bbc7c293dce25aaa8b6f4b4b842c993900
israelli <li18566230175@gmail.com>
2024-07-10 19:26:43 +0800

    feat(tree): setSelect & setChecked 参数调整

--- site/views/tree/config.ts
+++ site/views/tree/config.ts
@@ -294,7 +294,15 @@ export default [
         desc: '设置指定节点是否展开',
         params: '(item: any, isOpen = null, e: MouseEvent = null, fireEmit = true)',
       },
-      { name: 'setSelect', desc: '设置指定节点是否选中', params: '(item: any, selected = true, autoOpen = true)' },
+      {
+        name: 'setSelect',
+        desc: `* 设置节点选中状态
+           * @param nodes 选中节点，可以是多个
+           * @param selected 是否选中 default：true
+           * @param autoOpen 是否自动展开所有父级节点 default：true
+           * @param triggerEvent 是否触发抛出事件 false`,
+        params: '(item: any, selected = true, autoOpen = true, triggerEvent=false)',
+      },
       { name: 'asyncNodeClick', desc: '异步请求触发点击节点', params: '（item）' },
       { name: 'getData', desc: '获取当前树配置数据（经过内部处理的数据）', params: '（）' },
       { name: 'getParentNode', desc: '获取指定节点的父级节点', params: 'node: 当前节点' },
